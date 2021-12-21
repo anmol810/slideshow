d5b78b902d848fc73e6e1f366420adfa5a63c4c5
Anmol Chawla <anmolchawla810@gmail.com>
2021-12-22 04:40:39 +0530

    First commit

---
+++ slides/index.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll("[data-carousel-button]")
+buttons.forEach(button=>{
+      button.addEventListener("click",()=>{
+        const offset =  button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+        const slide = slides.querySelector("[ data-active]")
+        const activeSlide = slides.querySelector("[data-active]")
+        let newindex = [...slides.children].indexOf(activeSlide) + offset
+        if(newindex<0) newindex = slides.children.length - 1;
+         if(newindex>=slides.children.length) newindex = 0
+         slides.children[newindex].dataset.active  = true
+         delete activeSlide.dataset.active
+
+
+      })
+})
